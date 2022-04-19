e09ba994bef74a1e41f277fe7b09e69d4f0fbb5d
SushantShidurkar <sushant9322@gmail.com>
2022-04-19 13:28:58 +0000

    commit

--- api/src/main/java/com/abnamro/api/controller/ApiController.java
+++ api/src/main/java/com/abnamro/api/controller/ApiController.java
@@ -7,9 +7,27 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 public class ApiController{
 
+Logger logger= LoggerFactory.getLogger(RecipeController.class);
+@Autowired
+    private PersonService personService;
 
+    @PostMapping("/person")  
+	public ResponseEntity<String> createRecipe(@RequestBody Person person)  
+	{  
+		logger.info("createPerson called.");
+		if(null==person) {
+			recipeService.createRecipe(recipeDTO);  
+			return new ResponseEntity<>(HttpStatus.OK); 
+		}else {
+			logger.error("Id found in create recipe.");
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST); 
+		}
+	}
 }
